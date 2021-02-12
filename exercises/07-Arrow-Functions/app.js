cb42ba70258782b37b1d163eaba9054dc126173e
chackymeow <fabichasola@hotmail.com>
2021-02-12 17:39:07 +0000

    avance hasta ej9

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,4 +1,23 @@
+const rapid = (x) => {
+    console.log("entra a la funcion");
+    let almacenar = "";
+    let cambiar = "";
 
-   // From this line up Do not change code below
-   let str = "John";
-   console.log(rapid(str));
+    for (let i = 0; i < x.lenght; i++) {
+        if (true) {
+            console.log("pasa por vocales");
+        }
+        else {
+            cambiar = x[i];
+            cambiar = cambiar.toLowerCase();
+            almacenar = almacernar + cambiar;
+        }
+    }
+    console.log("sale de la funcion");
+    return "JHN";
+
+}
+
+// From this line up Do not change code below
+let str = "John";
+console.log(rapid(str));
